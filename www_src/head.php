6f05ae45bfcac878ca0ccf77f37b5a539658af44
Alexey <support@uvviewsoft.com>
2021-01-27 16:19:04 +0300

    revert

--- www_src/head.php
+++ www_src/head.php
@@ -1,5 +1,6 @@
     <a href="/cudatext/" class="title">CudaText</a> | 
     <a href="/uviewer" class="title">Universal Viewer</a> |
+    <a href="/synwrite/" class="title">SynWrite</a> | 
     <a href="/logviewer/" class="title">LogViewer</a>
     |
     <a href="/" class="title">For developers</a> <br>
